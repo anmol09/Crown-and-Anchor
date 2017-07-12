268aa07accae4990d70a542830eb7f25507c6995
Anmol Arora <a53arora@uwaterloo.ca>
2017-07-11 23:47:15 -0400

    Added money_won()

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
 import random
 
 
-
 def spin_wheel():
+	"""
+	Randomnly picks 3 objects from the dictionary, analogous to spinning a wheel with 
+	"""
 	result = []
 	options = { 1 : 'heart', 2 : 'spade' , 3 : 'diamond' , 4: 'club' , 5: 'crown', 6 : 'anchor' }
 
@@ -11,4 +13,45 @@ def spin_wheel():
 		result.append(options[value])
 
 	return result
+
+
+def money_won(bets,result):
+	
+	for k in range(len(bets)):
+		if result[0] == result[1] == result[2]== 'anchor'==bets[k][5]:
+			print("Player " + str(k) +" CISC 121 - Winner on Anchors")
+			exit()
+
+
+	total_money = [0,0,0,0,0,0]
+	profit_money = [0,0,0,0,0,0]
+
+
+	options = { 1 : 'heart', 2 : 'spade' , 3 : 'diamond' , 4: 'club' , 5: 'crown', 6 : 'anchor' }	
+	#options = { 'heart' : 1,  'spade' : 2 ,  'diamond' : 3 , 'club' : 4 , 'crown' : 5, 'anchor' : 6 }
+	
+	for i in range(len(bets)):
+		my_list = bets[i]
+
+		for j in range(1,7):
+			count = 0
+
+			if my_list[j-1]>0:
+
+				
+				if options[j] == result[0]:
+					count +=1
+				if options[j] == result[1]:
+					count +=1
+				if options[j] == result[2]:
+					count +=1
+
+
+				total_money[i] += count*my_list[j-1] + my_list[j-1]
+				profit_money[i] += count*my_list[j-1]
+
+		print("Player "+ str(i) +" won "+ str(profit_money[i]))
+
+	return total_money		
+
 
