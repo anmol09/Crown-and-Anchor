b860730b2605d81a43c2ef709c5ca2c7a9d9c1b6
nezar-d <nezardimitri@hotmail.com>
2017-07-11 21:33:05 -0400

    created main function
    simple player number prompt

---
+++ main.py
@@ -0,0 +1,14 @@
+def main:
+
+  player_num = int(input("# of players: ")) #prompts user for number of players
+  
+  print("\nEach player intially starts with 10$\nLet the game begin!\n")
+  
+
+  '''
+  a) ask for bets -- symbol and amount bet (or skip this round)
+  b) show the winning "slot"
+  c) indicate how much was won by each player
+  d) indicate how much money each player currently has
+  Repeat the above steps while at least one player has money left.
+  '''
